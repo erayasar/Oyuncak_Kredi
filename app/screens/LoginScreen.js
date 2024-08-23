d4e7f2bad3e67ba4a6a3739f8dbf805bb6b6caae
Eray  Asar <erayasar@Deeptechs-MacBook-Air.local>
2024-08-23 16:25:19 +0300

    Initial commit

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+import { Image, Text, Alert, View, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const LoginScreen = () => {
+    const [username, setUsername] = useState('');
+    
+}
